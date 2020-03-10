9e5ea4480f7f53b80a0d46605b93fee088bdd04b
rjxky <58586918+rjxky@users.noreply.github.com>
2020-03-10 17:16:54 +0100

    Add files via upload

---
+++ OpenXMLSolution/OpenXMLProject/FormMain.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace OpenXMLProject
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSimpleWordText = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSimpleWordText
+            // 
+            this.btnSimpleWordText.Location = new System.Drawing.Point(12, 12);
+            this.btnSimpleWordText.Name = "btnSimpleWordText";
+            this.btnSimpleWordText.Size = new System.Drawing.Size(332, 23);
+            this.btnSimpleWordText.TabIndex = 0;
+            this.btnSimpleWordText.Text = "SIMPLE WORD DOCUMENT";
+            this.btnSimpleWordText.UseVisualStyleBackColor = true;
+            this.btnSimpleWordText.Click += new System.EventHandler(this.btnSimpleWordText_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 315);
+            this.Controls.Add(this.btnSimpleWordText);
+            this.Name = "FormMain";
+            this.Text = "FormMain";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSimpleWordText;
+    }
+}
+
